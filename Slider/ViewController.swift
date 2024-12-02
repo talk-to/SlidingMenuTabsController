51db6fc518579bf1f4ce6e971f285174857aeb0b
Dinesh Kumar <dinesh.ku@flock.com>
2024-12-02 12:30:30 +0530

    Initial Commit

---
+++ Slider/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Slider
+//
+//  Created by Dinesh Kumar on 02/12/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
